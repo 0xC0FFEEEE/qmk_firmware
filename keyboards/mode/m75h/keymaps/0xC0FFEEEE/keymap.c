dcdd915753e19d97e68defb64a00f2457da648cf
0xC0FFEEEE <119874251+0xC0FFEEEE@users.noreply.github.com>
2024-10-27 17:07:54 +0000

    Mode Sonnet - Update Jiggler and Keymap

--- keyboards/mode/m75h/keymaps/0xC0FFEEEE/keymap.c
+++ keyboards/mode/m75h/keymaps/0xC0FFEEEE/keymap.c
@@ -22,8 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 enum custom_keycodes {
-  M_JIGL = SAFE_RANGE,
-  // Other custom keys...
+  M_JIGL = SAFE_RANGE
 };
 
 
@@ -37,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL , KC_LGUI, KC_LALT,                            KC_SPC,                            KC_RALT, TT(1)  ,          KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [1] = LAYOUT_ansi_blocker(
-        KC_TRNS, TO(0)  , TO(1)  , TO(2)  , TO(3)  ,          KC_TRNS, KC_TRNS, KC_TRNS, M_JIGL , KC_TRNS, KC_TRNS, KC_TRNS, QK_BOOT,          KC_TRNS,
+        KC_TRNS, TO(0)  , TO(1)  , TO(2)  , TO(3)  ,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, M_JIGL ,          KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_MUTE,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_VOLU,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_VOLD,
@@ -64,60 +63,39 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 bool is_mouse_jiggle_active = false;
-bool mouse_jiggle_direction = false; // used to alternate direction
-uint16_t mouse_jiggle_frequency = 1000; // how often to move the mouse (1 seconds)
-uint16_t mouse_jiggle_timer = 0;
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-      case M_JIGL:
-          if (record->event.pressed) {
-              is_mouse_jiggle_active = !is_mouse_jiggle_active;
-          }
-          break;
-    }
 
-    return true;
-}
-
-void matrix_scan_user(void) {
-    if (is_mouse_jiggle_active) {
-        if (timer_elapsed(mouse_jiggle_timer) > mouse_jiggle_frequency) {
-            mouse_jiggle_timer = timer_read();
-            if (mouse_jiggle_direction) {
-                tap_code(KC_MS_LEFT);
-            } else {
-                tap_code(KC_MS_RIGHT);
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+    if (record->event.pressed) {
+        static deferred_token token = INVALID_DEFERRED_TOKEN;
+        static report_mouse_t report = {0};
+        if (keycode == M_JIGL && is_mouse_jiggle_active) {
+        // If jiggler is currently running, stop when M_JIGL is pressed.
+        cancel_deferred_exec(token);
+        token = INVALID_DEFERRED_TOKEN;
+        report = (report_mouse_t){};  // Clear the mouse.
+        host_mouse_send(&report);
+        is_mouse_jiggle_active = false;
+        } else if (keycode == M_JIGL && !is_mouse_jiggle_active) {
+            is_mouse_jiggle_active = true;
+            uint32_t jiggler_callback(uint32_t trigger_time, void* cb_arg) {
+                // Deltas to move in a circle of radius 20 pixels over 32 frames.
+                static const int8_t deltas[32] = {
+                    0, -1, -2, -2, -3, -3, -4, -4, -4, -4, -3, -3, -2, -2, -1, 0,
+                    0, 1, 2, 2, 3, 3, 4, 4, 4, 4, 3, 3, 2, 2, 1, 0};
+                static uint8_t phase = 0;
+                // Get x delta from table and y delta by rotating a quarter cycle.
+                report.x = deltas[phase];
+                report.y = deltas[(phase + 8) & 31];
+                phase = (phase + 1) & 31;
+                host_mouse_send(&report);
+                return 16;  // Call the callback every 16 ms.
             }
-            mouse_jiggle_direction = !mouse_jiggle_direction;
+
+            token = defer_exec(1, jiggler_callback, NULL);  // Schedule callback.
         }
     }
-}
 
-
-void leader_start_user(void) {
-    // Do something when the leader key is pressed
-}
-
-void leader_end_user(void) {
-    if (leader_sequence_one_key(KC_F)) {
-        // Leader, f => Types the below string
-        SEND_STRING("QMK is awesome.");
-    } else if (leader_sequence_two_keys(KC_A, KC_C)) {
-        // Leader, a, c => Ctrl+A, Ctrl+C
-        SEND_STRING(SS_LCTL("a") SS_LCTL("c"));
-    } else if (leader_sequence_two_keys(KC_A, KC_V)) {
-        // Leader, a, v => Ctrl+A, Ctrl+V
-        SEND_STRING(SS_LCTL("a") SS_LCTL("v"));
-    // } else if (leader_sequence_two_keys(KC_A, KC_D)) {
-    //     // Leader, a, d => Ctrl+A, Del
-    //     SEND_STRING(SS_LCTL("a"));
-    //     SEND_STRING(KC_DEL);
-    // } else if (leader_sequence_three_keys(KC_D, KC_D, KC_S)) {
-    //     // Leader, d, d, s => Types the below string
-    //     SEND_STRING("https://start.duckduckgo.com\n");
-    // } else if (leader_sequence_two_keys(KC_A, KC_S)) {
-    //     // Leader, a, s => GUI+S
-    //     tap_code16(LGUI(KC_S));
-    }
+    // Other macros...
+    return true;
 }
