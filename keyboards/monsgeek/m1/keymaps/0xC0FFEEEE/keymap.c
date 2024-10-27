d0b5b3cacd44f1931f624d8116da37a2455a5c94
0xC0FFEEEE <119874251+0xC0FFEEEE@users.noreply.github.com>
2024-10-27 16:26:40 +0000

    Monsgeek M1 - Update Jiggle Code

--- keyboards/monsgeek/m1/keymaps/0xC0FFEEEE/keymap.c
+++ keyboards/monsgeek/m1/keymaps/0xC0FFEEEE/keymap.c
@@ -107,28 +107,36 @@ bool mouse_jiggle_direction = false; // used to alternate direction
 uint16_t mouse_jiggle_frequency = 1000; // how often to move the mouse (1 seconds)
 uint16_t mouse_jiggle_timer = 0;
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-      case M_JIGL:
-          if (record->event.pressed) {
-              is_mouse_jiggle_active = !is_mouse_jiggle_active;
-          }
-          break;
-    }
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+  if (record->event.pressed) {
+    static deferred_token token = INVALID_DEFERRED_TOKEN;
+    static report_mouse_t report = {0};
+    if (token) {
+      // If jiggler is currently running, stop when any key is pressed.
+      cancel_deferred_exec(token);
+      token = INVALID_DEFERRED_TOKEN;
+      report = (report_mouse_t){};  // Clear the mouse.
+      host_mouse_send(&report);
+    } else if (keycode == M_JIGL) {
 
-    return true;
-}
+      uint32_t jiggler_callback(uint32_t trigger_time, void* cb_arg) {
+        // Deltas to move in a circle of radius 20 pixels over 32 frames.
+        static const int8_t deltas[32] = {
+            0, -1, -2, -2, -3, -3, -4, -4, -4, -4, -3, -3, -2, -2, -1, 0,
+            0, 1, 2, 2, 3, 3, 4, 4, 4, 4, 3, 3, 2, 2, 1, 0};
+        static uint8_t phase = 0;
+        // Get x delta from table and y delta by rotating a quarter cycle.
+        report.x = deltas[phase];
+        report.y = deltas[(phase + 8) & 31];
+        phase = (phase + 1) & 31;
+        host_mouse_send(&report);
+        return 16;  // Call the callback every 16 ms.
+      }
 
-void matrix_scan_user(void) {
-    if (is_mouse_jiggle_active) {
-        if (timer_elapsed(mouse_jiggle_timer) > mouse_jiggle_frequency) {
-            mouse_jiggle_timer = timer_read();
-            if (mouse_jiggle_direction) {
-                tap_code(KC_MS_LEFT);
-            } else {
-                tap_code(KC_MS_RIGHT);
-            }
-            mouse_jiggle_direction = !mouse_jiggle_direction;
-        }
+      token = defer_exec(1, jiggler_callback, NULL);  // Schedule callback.
     }
+  }
+
+  // Other macros...
+  return true;
 }
